fb448d97de0af6ad6a4ecc0926acc35978e72f4a
Wenxuan Zhang <wenxuangm@gmail.com>
2024-12-25 21:25:25 +0800

    feawt: rewrite smartcolumn.lua

--- lua/smartcolumn.lua
+++ lua/smartcolumn.lua
@@ -1,115 +1,139 @@
+---@diagnostic disable: no-unknown
 local smartcolumn = {}
 
 local config = {
-   colorcolumn = "80",
-   disabled_filetypes = { "help", "text", "markdown" },
-   custom_colorcolumn = {},
-   scope = "file",
-   editorconfig = true,
+    colorcolumn = 80,
+    custom = {},
+    disabled_filetypes = { "markdown" },
+    editorconfig = true,
 }
 
-local function exceed(buf, win, min_colorcolumn)
-   local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true) -- file scope
-   if config.scope == "line" then
-      lines = vim.api.nvim_buf_get_lines(
-         buf,
-         vim.fn.line(".", win) - 1,
-         vim.fn.line(".", win),
-         true
-      )
-   elseif config.scope == "window" then
-      lines = vim.api.nvim_buf_get_lines(
-         buf,
-         vim.fn.line("w0", win) - 1,
-         vim.fn.line("w$", win),
-         true
-      )
-   end
+local disabled = true
 
-   local max_column = 0
+---@type (number|number[]|nil)
+local colorcolumns = nil
 
-   for _, line in pairs(lines) do
-      local success, column_number = pcall(vim.fn.strdisplaywidth, line)
+---@type number
+local colorcolumns_first = nil
 
-      if not success then
-         return false
-      end
+---@type string
+local colorcolumns_string = nil
 
-      max_column = math.max(max_column, column_number)
-   end
+local function colorcolumn_recompute(_ev)
+    disabled = vim.bo.buftype ~= "" -- special buffers
+        or vim.bo.filetype == "" -- no filetype
+        or vim.tbl_contains(config.disabled_filetypes, vim.bo.filetype) -- disabled filetypes
 
-   return not vim.tbl_contains(config.disabled_filetypes, vim.bo.ft)
-      and max_column > min_colorcolumn
+    if disabled then
+        return
+    end
+
+    colorcolumns = config.editorconfig
+        and vim.b[0].editorconfig
+        and tonumber(vim.b[0].editorconfig.max_line_length)
+
+    -- If the filetype is gitcommit, we don't want to use the editorconfig
+    if colorcolumns == nil or vim.bo.ft == "gitcommit" then
+        colorcolumns = config.custom[vim.bo.ft]
+            or colorcolumns
+            or config.colorcolumn
+    end
+
+    if type(colorcolumns) == "table" then
+        colorcolumns_first = colorcolumns[1]
+    else
+        colorcolumns_first = colorcolumns
+    end
+
+    if type(colorcolumns) == "table" then
+        colorcolumns_string = table.concat(colorcolumns, ",")
+    else
+        colorcolumns_string = tostring(colorcolumns)
+    end
 end
 
-local function colorcolumn_editorconfig(colorcolumns)
-   return vim.b[0].editorconfig
-         and vim.b[0].editorconfig.max_line_length ~= "off"
-         and vim.b[0].editorconfig.max_line_length
-      or colorcolumns
+local function should_display(buf, win)
+    local lines = vim.api.nvim_buf_get_lines(
+        buf,
+        vim.fn.line("w0", win) - 1,
+        vim.fn.line("w$", win),
+        true
+    )
+
+    local max_column = 0
+    for _, line in pairs(lines) do
+        local ok, column_number = pcall(vim.fn.strdisplaywidth, line)
+
+        if not ok then
+            return false
+        end
+
+        max_column = math.max(max_column, column_number)
+    end
+
+    return max_column > colorcolumns_first
 end
 
-local function update()
-   local buf_filetype = vim.api.nvim_buf_get_option(0, "filetype")
-   local colorcolumns
+local function colorcolumn_refresh(_ev)
+    if disabled then
+        return
+    end
 
-   if type(config.custom_colorcolumn) == "function" then
-      colorcolumns = config.custom_colorcolumn()
-   else
-      colorcolumns = config.custom_colorcolumn[buf_filetype]
-         or config.colorcolumn
-   end
-
-   if config.editorconfig then
-      colorcolumns = colorcolumn_editorconfig(colorcolumns)
-   end
-
-   local min_colorcolumn = colorcolumns
-   if type(colorcolumns) == "table" then
-      min_colorcolumn = colorcolumns[1]
-      for _, colorcolumn in pairs(colorcolumns) do
-         min_colorcolumn = math.min(min_colorcolumn, colorcolumn)
-      end
-   end
-   min_colorcolumn = tonumber(min_colorcolumn)
-
-   local current_buf = vim.api.nvim_get_current_buf()
-   local wins = vim.api.nvim_list_wins()
-   for _, win in pairs(wins) do
-      local buf = vim.api.nvim_win_get_buf(win)
-      if buf == current_buf then
-         local current_state = exceed(buf, win, min_colorcolumn)
-         if current_state ~= vim.b.prev_state then
-            vim.b.prev_state = current_state
-            if current_state then
-               if type(colorcolumns) == "table" then
-                  vim.wo[win].colorcolumn = table.concat(colorcolumns, ",")
-               else
-                  vim.wo[win].colorcolumn = colorcolumns
-               end
-            else
-               vim.wo[win].colorcolumn = ""
+    local current_buf = vim.api.nvim_get_current_buf()
+    local wins = vim.api.nvim_list_wins()
+    for _, win in pairs(wins) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if buf == current_buf then
+            local display = should_display(buf, win)
+            local current_cc = vim.wo[win].colorcolumn
+            if display and current_cc == "" then
+                vim.wo[win].colorcolumn = colorcolumns_string
+            elseif not display and current_cc ~= "" then
+                vim.wo[win].colorcolumn = ""
             end
-         end
-      end
-   end
+        end
+    end
 end
 
 function smartcolumn.setup(user_config)
-   user_config = user_config or {}
+    user_config = user_config or {}
 
-   for option, value in pairs(user_config) do
-      config[option] = value
-   end
+    for option, value in pairs(user_config) do
+        config[option] = value
+    end
 
-   local group = vim.api.nvim_create_augroup("SmartColumn", {})
-   vim.api.nvim_create_autocmd(
-      { "BufEnter", "CursorMoved", "CursorMovedI", "WinScrolled" },
-      {
-         group = group,
-         callback = update,
-      }
-   )
+    -- New created window will inherit the colorcolumn from the current window,
+    -- so we need to clear it.
+    vim.api.nvim_create_autocmd({ "WinNew" }, {
+        group = vim.api.nvim_create_augroup(
+            "SmartColumnClear",
+            { clear = false }
+        ),
+        callback = function()
+            vim.wo.colorcolumn = ""
+        end,
+    })
+
+    -- Recompute the colorcolumn string when a buffer is entered
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        group = vim.api.nvim_create_augroup(
+            "SmartColumnRecompute",
+            { clear = true }
+        ),
+        callback = colorcolumn_recompute,
+    })
+
+    -- Refresh the colorcolumn when the cursor moves
+    vim.api.nvim_create_autocmd(
+        { "CursorMoved", "CursorMovedI", "WinScrolled" },
+        {
+            group = vim.api.nvim_create_augroup(
+                "SmartColumnRefresh",
+                { clear = true }
+            ),
+            callback = colorcolumn_refresh,
+        }
+    )
 end
 
 return smartcolumn
